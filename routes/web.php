ffc296a743889fdc1ec09b551da167f56c0dcb8c
Aryaashari <aryaashari100@gmail.com>
2026-04-12 13:55:14 +0700

    feat: add proxy backend to get wilayah

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Models\PesertaBpjs;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\WilayahController;
 use App\Http\Controllers\PengajuanPekerjaRentanController;
 
 Route::get('/', function () {
@@ -16,6 +17,10 @@ Route::get('/cek-nik', function () {
 Route::get('/pengajuan', [PengajuanPekerjaRentanController::class, 'index']);
 Route::post('/pengajuan', [PengajuanPekerjaRentanController::class, 'store'])->name('pengajuan.store');
 Route::get('/pengajuan/file/{file}', [PengajuanPekerjaRentanController::class, 'showKtp'])->name('pengajuan.file')->middleware(['auth', 'admin', 'signed']);
+
+// API Wilayah (Backend Proxy)
+Route::get('/api/wilayah/kecamatan', [WilayahController::class, 'getDistricts']);
+Route::get('/api/wilayah/kelurahan/{districtCode}', [WilayahController::class, 'getVillages']);
 
 Route::post('/cek-peserta-bpjs', function (Request $request) {
     $peserta = PesertaBpjs::where('nik', $request->nik)->first();
